970db0a99972029ff68a39bf6907c0588808d695
LodingProgrammer <crazy5017@163.com>
2020-07-02 21:19:33 +0800

    删除无用文件

--- src/components/plugIns/vue-base-dialog/index.js
+++
@@ -1,7 +0,0 @@
-// module.exports = require('./lib/common');
-// console.log()
-import VueBaseDialog from './lib/VueBaseDialog.vue'
-VueBaseDialog.install = function(Vue) {
-  Vue.component(VueBaseDialog.name, VueBaseDialog);
-};
-export default VueBaseDialog
--- src/views/About.vue
+++ src/views/About.vue
@@ -11,7 +11,6 @@ export default {
   	return {}
   },
   created(){
-    // this.theGetUnit()
   },
   methods: {
     
